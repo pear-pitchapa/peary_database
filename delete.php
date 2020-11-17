bd582efe7102af400e751733d640a36cf8085b78
Bukorree <73693612+Bukorree@users.noreply.github.com>
2020-11-18 03:39:24 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,4 +1,18 @@
-<?php
+
+<html>
+
+  <head>
+    <title>ITF Lab</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>  
+  </head>
+
+<body>
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>  
+  
+  <?php
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'databaselabitf.mysql.database.azure.com', 'filmzz@databaselabitf', 'film8844@', 'filmDATAB', 3306);
 if (mysqli_connect_errno($conn))
@@ -10,7 +24,7 @@ $id=$_REQUEST['id'];
 echo $id;
 $sql = "DELETE FROM guestbook WHERE ID=$id"; 
 if ($conn->query($sql) === TRUE) {
-  echo "Record deleted successfully";
+  echo ('<div><h1 class="display-1">LAB Database Bukorree  Madthing</h1></div>');
 } else {
   echo "Error deleting record: " . $conn->error;
 }
@@ -18,7 +32,9 @@ if ($conn->query($sql) === TRUE) {
 $conn->close();
 if ($done)
 {
-    header("Location: https://labitf2.azurewebsites.net/show.php");
-    exit();
+    header("show.php");
+    exit;
 }
 ?>
+</body>
+</html>
