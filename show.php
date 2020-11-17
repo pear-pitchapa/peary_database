6a9b68ce6992074fe221819d5a9edc7fe6ac624f
Bukorree <73693612+Bukorree@users.noreply.github.com>
2020-11-18 01:41:35 +0700

    Update show.php

--- show.php
+++ show.php
@@ -4,6 +4,12 @@
     <title>ITF Lab</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>  
+    <script>
+      function dele() 
+      {
+        alert("Hi!");
+      }
+    </script>
   </head>
 
 <body>
@@ -66,7 +72,7 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
       <td><?php echo $Result['name'];?></td>
       <td><?php echo $Result['comment'];?></td>
       <td><?php echo $Result['link'];?></td>
-      <td><button type="button" class="btn btn-danger">Delete</button> <button type="button" class="btn btn-warning">Change</button></td>
+      <td><button type="button" onClick="dele()" class="btn btn-danger">Delete</button> <button type="button" class="btn btn-warning">Change</button></td>
     </tr>
 <?php
 }
