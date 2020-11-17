ed392750f5bc5c3a93009ddc9c0daa8bfe2207f3
Bukorree <73693612+Bukorree@users.noreply.github.com>
2020-11-18 02:48:30 +0700

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,13 @@
+<?php
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'databaselabitf.mysql.database.azure.com', 'filmzz@databaselabitf', 'film8844@', 'filmDATAB', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+$id=$_REQUEST['id'];
+$query = "DELETE FROM guestbook WHERE ID=$id"; 
+$result = mysqli_query($con,$query) or die ( mysqli_error());
+header("Location: show.php"); 
+?>
