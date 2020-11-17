42cd75ed9b5bd09c9b0bc9a5720efba93639eced
Bukorree <73693612+Bukorree@users.noreply.github.com>
2020-11-18 02:04:55 +0700

    Update show.php

--- show.php
+++ show.php
@@ -8,6 +8,12 @@
       function dele(param) 
       {
         <?php 
+          $conn = mysqli_init();
+          mysqli_real_connect($conn, 'databaselabitf.mysql.database.azure.com', 'filmzz@databaselabitf', 'film8844@', 'filmDATAB', 3306);
+          if (mysqli_connect_errno($conn))
+          {
+              die('Failed to connect to MySQL: '.mysqli_connect_error());
+          }
           $sql = "DELETE FROM guestbook WHERE ID=param";
           if (mysqli_query($conn, $sql)) {
               alert("yesss")
