38a62fd62860789a80acb623d3209efbeb51fe75
Bukorree <73693612+Bukorree@users.noreply.github.com>
2020-11-18 02:49:33 +0700

    Update show.php

--- show.php
+++ show.php
@@ -86,7 +86,7 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
       <td><?php echo $Result['name'];?></td>
       <td><?php echo $Result['comment'];?></td>
       <td><?php echo $Result['link'];?></td>
-      <td><?php echo('<button type="button" href="delete.php?id=<?php echo $Result['ID']; ?>" class="btn btn-danger" >Delete</button>'); ?> <button type="button" class="btn btn-warning">Change</button></td>
+      <td><?php echo('<button type="button" window.location.href="delete.php?id=<?php echo $Result['ID']; ?>" class="btn btn-danger" >Delete</button>'); ?> <button type="button" class="btn btn-warning">Change</button></td>
     </tr>
 <?php
 }
