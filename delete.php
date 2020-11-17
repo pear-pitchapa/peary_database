2af6b06bc57d95bc6cbf95645739d6613fbe3263
Bukorree <73693612+Bukorree@users.noreply.github.com>
2020-11-18 02:59:50 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,14 +1,3 @@
 <?php
-$conn = mysqli_init();
-mysqli_real_connect($conn, 'databaselabitf.mysql.database.azure.com', 'filmzz@databaselabitf', 'film8844@', 'filmDATAB', 3306);
-if (mysqli_connect_errno($conn))
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
-
 echo $id;
-$id=$_REQUEST['ID'];
-$query = "DELETE FROM guestbook WHERE ID=$id"; 
-$result = mysqli_query($con,$query) or die ( mysqli_error());
-header("Location: show.php"); 
 ?>
